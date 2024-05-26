e5333ee061a287785e223c42084a2b44bfc6714a
mezoinnit <mezomansour57@gmail.com>
2024-05-26 19:44:34 +0300

    Ui Changes: optimized the ui code a bit

--- app/src/main/java/com/example/mvvmproject/view/KotlinCoroutines/SimpleCoroutineAppScreen.kt
+++ app/src/main/java/com/example/mvvmproject/view/KotlinCoroutines/SimpleCoroutineAppScreen.kt
@@ -1,8 +1,8 @@
 package com.example.mvvmproject.view.KotlinCoroutines
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
@@ -22,22 +22,20 @@ import com.example.mvvmproject.viewmodel.CoroutineViewModel
 @Composable
 fun CoroutineTimer(vm: CoroutineViewModel = viewModel()) {
     var countdownValue by remember { mutableIntStateOf(0) }
-    LazyColumn(
+    Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
-        items(1) {
-            Text(text = vm.time.intValue.toString()) // timer value
-            TextField(
-                value = countdownValue.toString(),
-                onValueChange = { countdownValue = it.toIntOrNull() ?: 0 },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                keyboardActions = KeyboardActions(onDone = {
-                    vm.CoroutineTimer(countdownValue) // calls the coroutine logic
-                }),
-                singleLine = true
-            )
-        }
+        Text(text = vm.time.intValue.toString()) // timer value
+        TextField(
+            value = countdownValue.toString(),
+            onValueChange = { countdownValue = it.toIntOrNull() ?: 0 },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            keyboardActions = KeyboardActions(onDone = {
+                vm.CoroutineTimer(countdownValue) // calls the coroutine logic
+            }),
+            singleLine = true
+        )
     }
 }
