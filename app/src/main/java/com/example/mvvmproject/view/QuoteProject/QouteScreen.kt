7edeb848dabec5ca41b889993799d6fbff8c61cd
mezoinnit <mezomansour57@gmail.com>
2024-05-29 23:47:55 +0300

    QuoteProject update
    quick update - setting up the composable file

--- app/src/main/java/com/example/mvvmproject/view/QuoteProject/QouteScreen.kt
+++ app/src/main/java/com/example/mvvmproject/view/QuoteProject/QouteScreen.kt
@@ -1,3 +1,12 @@
 package com.example.mvvmproject.view.QuoteProject
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
 
+
+@Composable
+fun QuoteScreen(){
+    Column {
+
+    }
+}
