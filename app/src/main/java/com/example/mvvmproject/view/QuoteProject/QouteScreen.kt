f6eac9637df2010d1e5a920fffa42a2f96fa0e01
mezoinnit <mezomansour57@gmail.com>
2024-06-03 22:34:31 +0300

    quote Update
    done coding this file,

--- app/src/main/java/com/example/mvvmproject/view/QuoteProject/QouteScreen.kt
+++ app/src/main/java/com/example/mvvmproject/view/QuoteProject/QouteScreen.kt
@@ -7,21 +7,30 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mvvmproject.model.QuoteData
+import com.example.mvvmproject.viewmodel.QuoteViewModel
 
 
 @Composable
-fun QuoteScreen() {
+fun QuoteScreen(vm:QuoteViewModel = viewModel()) {
+
+    val quote = vm.quoteValue.collectAsState()
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "", style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold))
+        Text(text = quote.value.toMutableList()[0].quote, style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold))
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { vm.loadQuote() }) {
             Text(text = "Generate Quote!")
         }
 
