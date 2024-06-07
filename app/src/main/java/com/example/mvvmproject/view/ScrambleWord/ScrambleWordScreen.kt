481415229d4c18d9f954ab027d57e2c2ece8192e
mezoinnit <mezomansour57@gmail.com>
2024-06-07 15:35:30 +0300

    Scramble Update
    done setting up this file,

---
+++ app/src/main/java/com/example/mvvmproject/view/ScrambleWord/ScrambleWordScreen.kt
@@ -0,0 +1,21 @@
+package com.example.mvvmproject.view.ScrambleWord
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mvvmproject.ui.theme.MvvmProjectTheme
+
+@Composable
+fun ScrambleWordScreen(){
+    Column {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MvvmProjectTheme {
+        ScrambleWordScreen()
+    }
+}
