fb8213b349048baca353e859e1f0b01a23ba2519
mezoinnit <mezomansour57@gmail.com>
2024-06-09 23:53:05 +0300

    Scramble Update
    setting up the file.

--- app/src/main/java/com/example/mvvmproject/model/WordData.kt
+++ app/src/main/java/com/example/mvvmproject/model/WordData.kt
@@ -1,3 +1,21 @@
 package com.example.mvvmproject.model
 
 data class WordData(val word:String)
+
+val allWords: Set<String> =
+    setOf(
+        "animal",
+        "auto",
+        "anecdote",
+        "alphabet",
+        "all",
+        "awesome",
+        "arise",
+        "balloon",
+        "basket",
+        "bench",
+        // ...
+        "zoology",
+        "zone",
+        "zeal"
+    )
