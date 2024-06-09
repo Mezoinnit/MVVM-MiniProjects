3374cd6d8912432de1901c48427b8de4236ff583
mezoinnit <mezomansour57@gmail.com>
2024-06-09 22:52:43 +0300

    Scramble Update
    setting up the file.

--- app/src/main/java/com/example/mvvmproject/view/ScrambleWord/ScrambleWordScreen.kt
+++ app/src/main/java/com/example/mvvmproject/view/ScrambleWord/ScrambleWordScreen.kt
@@ -1,6 +1,8 @@
 package com.example.mvvmproject.view.ScrambleWord
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mvvmproject.ui.theme.MvvmProjectTheme
@@ -8,7 +10,10 @@ import com.example.mvvmproject.ui.theme.MvvmProjectTheme
 @Composable
 fun ScrambleWordScreen(){
     Column {
-
+        Text(text = "word")
+        Button(onClick = { /*TODO:ScrambleWordLogic*/ }) {
+            Text(text = "Text")
+        }
     }
 }
 
