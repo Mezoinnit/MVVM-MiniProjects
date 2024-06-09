5ee75337881f6d173bfedb5a333a32590bb9e56e
mezoinnit <mezomansour57@gmail.com>
2024-06-09 23:51:14 +0300

    Scramble Update
    setting up the file.

--- app/src/main/java/com/example/mvvmproject/viewmodel/ScrambleWordViewModel.kt
+++ app/src/main/java/com/example/mvvmproject/viewmodel/ScrambleWordViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.mvvmproject.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class ScrambleWordViewModel:ViewModel(){
+
+    val _scrambledWord = MutableStateFlow("")
+
+    val word = _scrambledWord.asStateFlow()
+
+    val coroutine = {
+
+    }
 
 }
